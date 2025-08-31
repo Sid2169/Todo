181225cf9aaf7699d0e3e602611fc50562b09e61
Sid2169 <siddharthasingh21109572@gmail.com>
2025-09-01 01:30:23 +0530

    feat(projects): add Project class and supporting modules
    - Project.js: declare Project class
    - createProject.js: handle project creation
    - projectList.js: manage list of projects

---
+++ src/modules/Project.js
@@ -0,0 +1,7 @@
+export  class Project {
+    constructor(projectName, projectDescription) {
+        this.projectName = projectName;
+        this.projectDescription = projectDescription;
+        this.tasks = [];
+    }
+}   
---
+++ src/modules/createProject.js
@@ -0,0 +1,22 @@
+import { Project } from "./Project.js";
+import { projectList } from "./projectList.js";
+
+//Create Project and update project list in sidebar
+export const createProject = (projectName, projectDescription) => {
+    const project = new Project(projectName, projectDescription);
+    projectList.push(project);
+    const projectItem = document.createElement('li');
+    projectItem.classList.add('project-item');
+    projectItem.innerHTML = `
+        <span>${projectName}</span>
+    `;
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add('delete-project');
+    deleteButton.innerHTML = '🗑️';
+    deleteButton.addEventListener('click', () => {
+        projectList.splice(projectList.indexOf(project), 1);
+        projectItem.remove();
+    });
+    projectItem.appendChild(deleteButton);
+    document.getElementById('projectList').appendChild(projectItem);
+}
